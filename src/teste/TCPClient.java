e4bf5f3a155d83d8ba4ce8c27af45a5128b467a8
Davi Rezende <davijf_404@hotmail.com>
2021-01-20 22:19:23 -0300

    davi: clean code e pequenos acertos

--- src/teste/TCPClient.java
+++ src/teste/TCPClient.java
@@ -1,58 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package teste;
-
-/**
- *
-  * @author Pedro Cotta Badaro 201776014
- */
-
-    import java.io.*; 
-    import java.net.*; 
+//@author Pedro Cotta Badaro - 201776014
+//@author Davi Rezende Domingues - 201765505ab
+import java.io.*;
+import java.net.*;
 
 public class TCPClient {
-    public static void main(String argv[]) throws Exception 
-    { 
-        String sentence; 
+    public static void main(String argv[]) throws Exception {
+        String sentence;
         String modifiedSentence;
-     
-        Socket clientSocket = new Socket("192.168.0.131", 6789);
-            while(true){
-             
-                BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
-                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
-                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-                //ENVIAR MSG
-                System.out.println("INSERIR MENSAGEM:\n");
-                sentence = inFromUser.readLine(); 
-                
-                if(sentence.contains("ENCERRAR")){
-                    System.out.println("ENTROU IF ENCERRAR 1");
-                    clientSocket.close();
-                    System.exit(1);
-                }
-                outToServer.writeBytes(sentence + '\n'); 
-                
-                
-                modifiedSentence = inFromServer.readLine(); 
-                //MSG RECEBIDA
-                if(modifiedSentence.contains("ENCERRAR")){
-                    System.out.println("ENTROU IF ENCERRAR 2");
-                    clientSocket.close();
-                    System.exit(1);
-                }
-                System.out.println("FROM SERVER: " + modifiedSentence);
-
-                
-        }
-           
-   
-    } 
-
-
+        Socket clientSocket = new Socket("192.168.2.107", 6789);
         
+        while (true) {
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            //captura a mensagem a ser enviada
+            System.out.println("INSERIR MENSAGEM: ");
+            sentence = inFromUser.readLine();
+            //verifica se eh o codigo de encerramento
+            if (sentence.contains("ENCERRAR")) {
+                System.out.println("ENCERRANDO CHAT");
+                clientSocket.close();
+                System.exit(1);
+            }
+            //envia a mensagem
+            outToServer.writeBytes(sentence + '\n');
+
+            //captura a mensagem que chega do servidor
+            modifiedSentence = inFromServer.readLine();
+            //analisa mensagem recebibda
+            if (modifiedSentence.contains("ENCERRAR")) {
+                System.out.println("ENCERRANDO CHAT");
+                clientSocket.close();
+                System.exit(1);
+            }
+            //imprime mensagem recebida
+            System.out.println("FROM SERVER: " + modifiedSentence);
+        }
+    }
 }
--- src/teste/TCPServer.java
+++ src/teste/TCPServer.java
@@ -1,56 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package teste;
+//@author Pedro Cotta Badaro 201776014
+//@author Davi Rezende Domingues - 201765505ab
+import java.io.*;
+import java.net.*;
 
-/**
- *
- * @author Pedro Cotta Badaro 201776014
- */
-import java.io.*; 
-import java.net.*; 
-
-class TCPServer { 
-
-  public static void main(String argv[]) throws Exception 
-    { 
-        String clientSentence; 
-        String capitalizedSentence; 
-        String serverSentence; 
-      ServerSocket welcomeSocket = new ServerSocket(6789); 
+class TCPServer {
+    public static void main(String argv[]) throws Exception {
+        String clientSentence;
+        String serverSentence;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
         System.out.println("SERVIDOR INICIADO NA PORTA 6789");
         Socket connectionSocket = welcomeSocket.accept();
-      while(true) { 
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
-            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(System.in)); 
-	   DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream()); 
-            //MSG RECEBIDA
-           clientSentence = inFromClient.readLine(); 
-           if(clientSentence.contains("ENCERRAR")){
-                     System.out.println("ENTROU IF ENCERRAR 3");
-               welcomeSocket.close();
-               System.exit(1);
-           }
-           System.out.println("FROM CLIENT "+clientSentence);
-           
-           //Enviar MSG
-           System.out.println("Digitar mensagem:\n");
-           serverSentence = inFromServer.readLine();
-           capitalizedSentence = serverSentence + '\n'; 
-           
-           if(capitalizedSentence.contains("ENCERRAR")){
-                     System.out.println("ENTROU IF ENCERRAR 4");
-               welcomeSocket.close();
-               System.exit(1);
-           }
-           
-           outToClient.writeBytes(capitalizedSentence); 
-           
+        
+        while (true) {
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(System.in));
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
             
-         
-        } 
-    } 
-} 
- 
+            //captura mensagem recebida do client
+            clientSentence = inFromClient.readLine();
+            //analisa mensagem recebida
+            if (clientSentence.contains("ENCERRAR")) {
+                System.out.println("ENCERRANDO CHAT");
+                welcomeSocket.close();
+                System.exit(1);
+            }
+            //imprime mensagem recebida
+            System.out.println("FROM CLIENT: " + clientSentence);
+
+            //captura mensagem pra enviar pro client
+            System.out.println("INSERIR MENSAGEM: ");
+            serverSentence = inFromServer.readLine();
+            //analisa mensagem a ser enviada
+            if (serverSentence.contains("ENCERRAR")) {
+                System.out.println("ENCERRANDO CHAT");
+                welcomeSocket.close();
+                System.exit(1);
+            }
+            //envia mensagem pro client
+            outToClient.writeBytes(serverSentence);
+        }
+    }
+}
